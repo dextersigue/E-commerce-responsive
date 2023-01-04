5d832b34daafaede6833875896c54c3ddf3aecb1
Dexter Sigue <dextersigue@gmail.com>
2023-01-04 15:00:26 +0800

    added the code for the image to show using Javascript

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ if(close){
         nav.classList.remove('active')
     })
 }
+
+//////////////////////////////// 
+//script of single product
+// if click the small show in the main image
+
+
+    let mainImg = document.getElementById('mainImg');
+    let smallImg = document.getElementsByClassName('smallImg');
+
+    smallImg.addEventListener("click", showImage);
+
+    function showImage(){
+        for(j=0; j<smallImg.length; j++){
+         mainImg.src = smallImg[j].src;
+    }
+  
+    for(i = 0; i < smallImg.length; i++){
+        smallImg[i].onclick = function(){
+               showImage();
+            }
+        }
+    }
+
+
+
+
+
+// smallImg[i].onclick = function(){
+//     mainImg.src = smallImg[i].src;
+// }
+
+// smallImg[1].onclick = function(){
+//     mainImg.src = smallImg[1].src;
+// }
+
+// smallImg[2].onclick = function(){
+//     mainImg.src = smallImg[2].src;
+// }
+
+// smallImg[3].onclick = function(){
+//     mainImg.src = smallImg[3].src;
+// }
+
